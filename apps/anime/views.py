a24ac5f5a09c902c0107e1d2b6177c73d81fcbc9
TemirbolatMaratuly <snake2009003@mail.ru>
2022-04-09 21:32:06 +0600

    Added vscode settings, generate data for users, APIView for users alng with the url in auths

--- apps/anime/views.py
+++ apps/anime/views.py
@@ -1,9 +1,9 @@
-from rest_framework import permissions
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import (
+    authentication,
+    permissions,
+)
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-# from django.db.models import QuerySet
 
 from auths.models import CustomUser
 
@@ -16,14 +16,15 @@ class ListUsers(APIView):
     * Only superusers are able to access this view.
     """
 
+    # authentication_classes: tuple = (
+    #     authentication.TokenAuthentication,
+    # )
+    authentication_classes: tuple = ()
     permission_classes: tuple = (
         permissions.IsAdminUser,
     )
 
-    def get(self, request) -> Response:  # noqa
+    def get(self, request) -> Response:
         """Return list of all users."""
-
-        data: list = [
-            user.password for user in CustomUser.objects.all()
-        ]
+        data: list = [user.email for user in CustomUser.objects.all()]
         return Response(data)
